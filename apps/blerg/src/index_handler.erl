9d69a6a36fc29e487da2718b20802666e1215ee2
Roger Lipscombe <roger@differentpla.net>
2013-12-30 19:09:53 +0000

    Use a template for the index page.

--- apps/blerg/src/index_handler.erl
+++ apps/blerg/src/index_handler.erl
@@ -6,8 +6,8 @@ init(_Type, Req, _Opts) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    Headers = [{<<"content-type">>, <<"text/plain">>}],
-    Body = <<"Hello World!">>,
+    Headers = [{<<"content-type">>, <<"text/html">>}],
+    {ok, Body} = index_dtl:render(),
     {ok, Req2} = cowboy_req:reply(200, Headers, Body, Req),
     {ok, Req2, State}.
 
