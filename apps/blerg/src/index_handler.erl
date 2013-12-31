771265935894bb8d44e0b5c96b67ae26be9a3ccc
Roger Lipscombe <roger@differentpla.net>
2013-12-31 16:12:23 +0000

    Figure out the syntax for nested stuff.

--- apps/blerg/src/index_handler.erl
+++ apps/blerg/src/index_handler.erl
@@ -6,11 +6,9 @@ init(_Type, Req, _Opts) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    Site = {name, "Roger's Blog"},
     Headers = [{<<"content-type">>, <<"text/html">>}],
     Posts = posts:index(),
-    lager:info("Posts: ~p", [Posts]),
-    {ok, Body} = index_dtl:render([{site, Site}, {posts, Posts}]),
+    {ok, Body} = index_dtl:render([{site, [{name, "Foo"}]}, {posts, Posts}]),
     {ok, Req2} = cowboy_req:reply(200, Headers, Body, Req),
     {ok, Req2, State}.
 
