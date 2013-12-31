667e0f311b698c111ce78a5109f7ac8335e755e2
Roger Lipscombe <roger@differentpla.net>
2013-12-31 20:25:30 +0000

    Only display most recent 10 posts.

--- apps/blerg/src/posts.erl
+++ apps/blerg/src/posts.erl
@@ -4,7 +4,9 @@
 index() -> 
     {ok, _Cols, Rows} = blerg_db:equery("SELECT p.title, u.name AS author, p.created_at, p.body
                                          FROM posts p
-                                       JOIN users u ON p.author_id = u.id"),
+                                       JOIN users u ON p.author_id = u.id
+                                       ORDER BY p.created_at DESC
+                                       LIMIT 10"),
     % We want a proplist, rather than simple columns, so...
     % TODO: Consider iterating over the returned columns and doing it that way...
     [[{title, Title}, {author, Author}, {created_at, CreatedAt}, {body, Body}] || {Title, Author, CreatedAt, Body} <- Rows].
