0eddb070e36ed616f696c9e07e764e16fe787c43
Roger Lipscombe <roger@differentpla.net>
2014-01-18 09:19:51 +0000

    Another couple of aliases.

--- migrations/20140118-002-update-aliases.sql
+++ migrations/20140118-002-update-aliases.sql
@@ -2,4 +2,5 @@ INSERT INTO aliases(alias, post_id) VALUES('/2013/04/18/MessageSecurityException
 INSERT INTO aliases(alias, post_id) VALUES('/post/UnarmpR1CYUQAAAD/lego-storage', 42);
 INSERT INTO aliases(alias, post_id) VALUES('/2013/04/17/How-to-do-NET-Reflection-in-PowerShell', 4);
 INSERT INTO aliases(alias, post_id) VALUES('/2013/02/06/What-is-this-', 39);
-
+INSERT INTO aliases(alias, post_id) VALUES('/b/2013/11/06/what-is-this-', 39);
+INSERT INTO aliases(alias, post_id) VALUES('/b/2013/41/13/finding-iis-worker-processes', 19);
