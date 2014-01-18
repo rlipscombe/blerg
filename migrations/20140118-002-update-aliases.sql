3b4178a1b101c3388f8c6698bee44a706f42899a
Roger Lipscombe <roger@differentpla.net>
2014-01-18 09:13:16 +0000

    More aliases.

---
+++ migrations/20140118-002-update-aliases.sql
@@ -0,0 +1,5 @@
+INSERT INTO aliases(alias, post_id) VALUES('/2013/04/18/MessageSecurityException/', 26);
+INSERT INTO aliases(alias, post_id) VALUES('/post/UnarmpR1CYUQAAAD/lego-storage', 42);
+INSERT INTO aliases(alias, post_id) VALUES('/2013/04/17/How-to-do-NET-Reflection-in-PowerShell', 4);
+INSERT INTO aliases(alias, post_id) VALUES('/2013/02/06/What-is-this-', 39);
+
