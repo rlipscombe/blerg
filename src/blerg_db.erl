b9bfcab54a9678ae31ab38264b0cd88b2d37a623
Roger Lipscombe <roger@differentpla.net>
2014-07-08 08:04:58 +0100

    Bump DB timeouts.

--- src/blerg_db.erl
+++ src/blerg_db.erl
@@ -2,14 +2,16 @@
 -export([equery/1, equery/2]).
 
 -define(POOL_NAME, blerg_db).
+-define(CHECKOUT_TIMEOUT_MS, 30000).
+-define(CALL_TIMEOUT_MS, 30000).
 
 equery(Stmt) ->
     equery(Stmt, []).
 
 equery(Stmt, Params) ->
     log_result(poolboy:transaction(?POOL_NAME, fun(W) ->
-                gen_server:call(W, {equery, Stmt, Params})
-            end)).
+                gen_server:call(W, {equery, Stmt, Params}, ?CALL_TIMEOUT_MS)
+            end, ?CHECKOUT_TIMEOUT_MS)).
 
 log_result(Result) ->
     case element(1, Result) of
